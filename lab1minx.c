e20017ff765e9ea57859151fb5b670dce600e5bf
s6704062617237 <s6704062617237@email.kmutnb.ac.th>
2024-11-28 18:15:32 +0700

    Scanf

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 int main() {
-	int x=10;
-	int y=1000;
+	int x;
+	int y;
+	printf("n1:");
+	scanf("%d",&x);
+	printf("n2:");
+	scanf("%d",&y);
 	if(x>y){
 		printf("num1 is greater than num2");
-	}
-	if(x<y){
+	}else{
 		printf("num2 is greater than num1");
 	}
 }
