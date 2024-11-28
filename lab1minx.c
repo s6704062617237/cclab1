d487686304fa854cc97f5befe818447581b6d31f
s6704062617237 <s6704062617237@email.kmutnb.ac.th>
2024-11-28 18:23:17 +0700

    Scanf but n1 n2:

--- lab1minx.c
+++ lab1minx.c
@@ -3,10 +3,8 @@
 int main() {
 	int x;
 	int y;
-	printf("n1:");
-	scanf("%d",&x);
-	printf("n2:");
-	scanf("%d",&y);
+	printf("n1 n2:");
+	scanf("%d%d",&x,&y);
 	if(x>y){
 		printf("num1 is greater than num2");
 		//diff of x and y
